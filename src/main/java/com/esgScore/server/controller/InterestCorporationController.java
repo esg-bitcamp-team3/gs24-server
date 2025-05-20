07ee43d373af84149cda87e347b91e4a68048c88
tjdus <dusml0277@gmail.com>
2025-05-20 14:33:08 +0900

    rename path

--- src/main/java/com/esgScore/server/controller/InterestCorporationController.java
+++ src/main/java/com/esgScore/server/controller/InterestCorporationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/interestCorporation")
+@RequestMapping("/api/interest-corporations")
 @RequiredArgsConstructor
 @Slf4j
 public class InterestCorporationController {
